19ddff4e8426bdd7ccbbce78daace2994060cea1
ayal <ayalgelles@gmail.com>
2020-01-31 22:26:20 +0200

    Updates

---
+++ precache-manifest.11d356f300b8ffe0849ea9d53187f1e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bbdecfca9ad32425ed25467f42ddec",
+    "url": "/zman/index.html"
+  },
+  {
+    "revision": "3ff09d0372129a90d0f6",
+    "url": "/zman/static/css/main.4fb7eeef.chunk.css"
+  },
+  {
+    "revision": "d03e3248f63b29469f02",
+    "url": "/zman/static/js/2.607715c1.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/zman/static/js/2.607715c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ff09d0372129a90d0f6",
+    "url": "/zman/static/js/main.baab51d4.chunk.js"
+  },
+  {
+    "revision": "d83b70e5400ac4e3d153",
+    "url": "/zman/static/js/runtime-main.613377b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zman/static/media/logo.5d5d9eef.svg"
+  }
+]);
