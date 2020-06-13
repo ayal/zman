116bf1e99d3a9fc494168d065396368ec9e4da04
ayal <ayalg@wix.com>
2020-06-13 12:59:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import TimerSet from './TimerSet.js';
+import * as NoSleep from 'nosleep.js';
 
 import {
   BrowserRouter as Router,
 } from "react-router-dom";
+
+var noSleep = new NoSleep();
+document.addEventListener('click', function enableNoSleep() {
+  document.removeEventListener('click', enableNoSleep, false);
+  console.log('enabling no sleep');
+  noSleep.enable();
+}, false);
 
 
 function App() {  
