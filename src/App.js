86aae79347abd247c0bab10fa428bede79a273ae
ayal <ayalg@wix.com>
2020-06-13 11:52:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TimerSet from './TimerSet.js';
 
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
 } from "react-router-dom";
 
 
