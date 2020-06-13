1276267e2c40a51d8c752a76ff6cf0244168b27d
ayal <ayalg@wix.com>
2020-06-13 21:10:23 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,1 @@
-import * as NoSleep from 'nosleep.js';
-var noSleep = new NoSleep();
-document.addEventListener('click', function enableNoSleep() {
-  document.removeEventListener('click', enableNoSleep, false);
-  console.log('enabling no sleep');
-  noSleep.enable();
-}, false);
+
