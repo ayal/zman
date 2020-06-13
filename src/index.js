c693996a4cd768baa25fc62f157c12c52664afa5
ayal <ayalg@wix.com>
2020-06-13 11:47:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import utils from './utils';
 
 import * as serviceWorker from './serviceWorker';
 
