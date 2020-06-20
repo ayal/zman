e968ef8b63cbf85d8f1f34dc63bcf8ec036f4a4e
ayal <ayalg@wix.com>
2020-06-20 17:24:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+
 import NoSleep from './NoSleep';
 
 window.noSleep = null;
@@ -17,7 +19,6 @@ document.addEventListener('click',  () => {
 }, false);
 
 
-import App from './App';
 
 //import * as serviceWorker from './serviceWorker';
 
