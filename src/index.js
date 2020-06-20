097047eb4a3950ccd40d41cd0e309ba2fcf68f96
ayal <ayalg@wix.com>
2020-06-20 15:18:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 
 
@@ -18,11 +18,11 @@ document.addEventListener('click',  () => {
 }, false);
 
 
-//import App from './App';
+import App from './App';
 
 //import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
